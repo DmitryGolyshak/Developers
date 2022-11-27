8648b7f59e1b9ec6e80f7084a845babec13f1042
DmitryGolyshak <dmgolyshak@gmail.com>
2022-11-27 21:34:54 +0300

    optimization and fix

--- Task.js
+++ Task.js
@@ -1,23 +1,44 @@
+//функция, разбивающая число на цифры
+function getBreakTheNumber(number){
+    while (number >= 1) {
+        if (number % 10 == 0) {
+            number = number / 10;
+            breakTheNumber.push(0);
+        } else if (number < 10) {
+            breakTheNumber.push(number);
+            break;
+        } else {
+            breakTheNumber.push(number % 10);
+            number = ((number - (number % 10)) / 10);
+        }
+    }
+    return breakTheNumber;
+}
+let breakTheNumber = [];
+
 //1.1
-function calcNod(a, b) {
+function calcNod(number1, number2) {
     let NOD;
-    for (let c = 1; a / c >= 1 && b / c >= 1; c++) {
-        if (a % c == 0 && b % c == 0) {
-            NOD = c;
+    for (let divider = 1; number1 / divider >= 1 && number2 / divider >= 1; divider++) {
+        if (number1 % divider == 0 && number2 % divider == 0) {
+            NOD = divider;
         }
     }
     return NOD;
-}
+}//исправлены названия переменных.
 
 //1.2
 function getIsPrimeNum(a) {
-    let b = 2;//*
-    while (a % 2 !== 0 && a % b !== 0 && b <= a) {//оптимизация
-        ++b;
+    let b = 2;
+    while (a % 2 !== 0 && a % b !== 0 && b <= a) {
     }
-
+    b++;
     return b == a;
 }
+
+function getIsPrimeNum(a) {
+    return (a == 2 || a == 3 || a == 5 || a == 7 || a % 2 !== 0 && a % 10 >= 0 && a % 3 !== 0 && a % 5 !== 0 && a % 7 !== 0 && a % 9 !== 0);
+}//уменьшено потребление ресурсов, но простые числа до 10 - ?
 
 //1.3
 function getSumNumber(a) {
@@ -40,6 +61,14 @@ function getSumNumber(a) {
     return ({ sum, count });
 }
 
+function getSumAndNumOfNumber(number) {
+    let result = [];
+    getBreakTheNumber(number);
+    result.push(breakTheNumber.reduce((accumulator, currentValue) => accumulator + currentValue));
+    result.push(breakTheNumber.length);
+    return result;
+}//отдельная функция для разбивки чисел, решение через метод массива.
+
 //1.4
 function calcPerfectNum(a) {
     let arr = [];
@@ -60,6 +89,16 @@ function calcPerfectNum(a) {
     }
 }
 
+function calcPerfectNum(number) {
+    let arr = [];
+    for (let divider = 1; number / divider > 1; divider++) {
+        if (number % divider == 0) {
+            arr.push(divider);
+        }
+    }
+    return (Object.is(arr.reduce((accumulator, currentValue) => accumulator + currentValue), number));
+}//исправлены названия, while заменен на reduce
+
 //1.5
 function getSumCube(x) { //для всех чисел
     let a = "";
@@ -74,6 +113,14 @@ function getSumCube(x) { //для всех чисел
     }
     return a;
 }
+
+function getNumEqualSumCubeDigits(N) {
+    for(let number = 10; number < N; number++){
+        getBreakTheNumber(number);
+        breakTheNumber.forEach((item) => item ** 3);
+    }
+    return breakTheNumber;
+}//еще доделываю
 
 //1.6
 function insertNine(a) {//использовать методы массивов, названия переменных, вернуть число
@@ -102,19 +149,32 @@ function insertNine(a) {//использовать методы массивов
     return (b);
 }
 
+function insertNine(number) {
+    getBreakTheNumber(number);
+    const newArray = breakTheNumber.reduce(function (previousValue, currentValue) {
+        previousValue.push(currentValue);
+        previousValue.push(9);
+        return previousValue;
+    }, [])
+    newArray.pop();
+    newArray.reverse();
+    return +newArray.join('');
+}//отдельная функция для разбивки чисел, решение через метод массива, возвращается число.
+
+
 //1.7
-function calcNumSolutions(n) {
+function calcNumSolutions(number) {
     let x, y;
-    let c = 0;
-    for (x = 1; x < n; ++x) {
-        for (y = 1; y < n; ++y) {
-            if (x * x + y * y < n) {
-                c = c + 1;
+    let numSolutions = 0;
+    for (x = 1; x < number; ++x) {
+        for (y = 1; y < number; ++y) {
+            if (x * x + y * y < number) {
+                ++numSolutions;
             }
         }
     }
-    return (c);
-}
+    return (numSolutions);
+}//исправлены названия переменных.
 
 //1.8
 function doubleMaxNum(a) {//разбивка цифер как отдельная функция
@@ -156,41 +216,11 @@ function doubleMaxNum(a) {//разбивка цифер как отдельна�
     return (num);
 }
 
-//1.9
-function getNum() {
-    let arr = [];
-    for (let i = 1; i <= 9; i++) {
-        for (let j = 0; j <= 9; j++) {
-            if (i != j) {
-                for (let k = 0; k <= 9; k++) {
-                    if ((k != i) & (k != j)) {
-                        let a = i * 100 + j * 10 + k;
-                        arr.push(a);
-                    }
-                }
-            }
-        }
-    }
-    return arr;
-}
-
-function doubleMaxNum(a) {
-    let arr = [];
-    while (a >= 1) {
-        if (a % 10 == 0) {
-            a = a / 10;
-            arr.push(0);
-        } else if (a < 10) {
-            arr.push(a);
-            break;
-        } else {
-            arr.push(a % 10);
-            a = (a - (a % 10)) / 10;
-        }
-    }
-    arr.reverse();
-    let max = Math.max(...arr);
-    const newArray = arr.reduce(function (previousValue, currentValue) {
+function doubleMaxNum(number) {
+    getBreakTheNumber(number);
+    breakTheNumber.reverse();
+    let max = Math.max(...breakTheNumber);
+    const newArray = breakTheNumber.reduce(function (previousValue, currentValue) {
         if (currentValue == max) {
             previousValue.push(currentValue);
         }
@@ -201,4 +231,22 @@ function doubleMaxNum(a) {
     // previousValue = previousValue.join('');
     return newArray.join('');
 }
-doubleMaxNum(564638642);
+doubleMaxNum(564638642);//отдельная функция для разбивки чисел, изменены названия переменных.
+
+//1.9
+function getNumWithoutRepeat() {
+    let result = [];
+    for (let hundreds = 1; hundreds <= 9; hundreds++) {
+        for (let tens = 0; tens <= 9; tens++) {
+            if (hundreds != tens) {
+                for (let units = 0; units <= 9; units++) {
+                    if ((units != hundreds) & (units != tens)) {
+                        let numWithoutRepeat = hundreds * 100 + tens * 10 + units;
+                        result.push(numWithoutRepeat);
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}//изменены названия переменных.
